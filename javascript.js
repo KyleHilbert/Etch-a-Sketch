18efa311105457557f747742227d33d92c476791
Kyle Hilbert <104146726+KyleHilbert@users.noreply.github.com>
2022-10-13 15:51:29 -0400

    Added reset button

--- javascript.js
+++ javascript.js
@@ -2,25 +2,29 @@ const flexContainer = document.querySelector(".flexContainer");
 
 const container = document.querySelector(".container");
 container.style.display = "grid";
-container.style.width = "640px";
+container.style.width = "600px";
+container.style.height = "600px";
 container.style.background = "#e8a87c";
 container.style.border = "6px solid #E27D60";
 
 function makeGrid() {
-  //Ask for grid size, one number EX: 16 would 16x16
-  //for loop making divs for grid
+  let count = 0;
 
-  //let count = prompt("Enter grid size");
-  let count = 16;
+  do {
+    count = prompt("Enter grid size");
+    count = parseInt(count);
+  } while (isNaN(count) || count > 100);
 
-  container.style.gridTemplateColumns = "repeat(" + count + ",40px)";
+  count = Math.floor(Math.abs(count));
+
+  container.style.gridTemplateColumns = "repeat(" + count + ",1fr";
 
   for (let i = 0; i < count * count; i++) {
     const gridSquare = document.createElement("div");
     container.appendChild(gridSquare);
     gridSquare.style.textAlign = "center";
-    gridSquare.style.height = "40px";
-    gridSquare.style.width = "40px";
+    gridSquare.style.height = "((1/count)*100)%";
+    gridSquare.style.width = "((1/count)*100)%";
     gridSquare.style.border = "1px solid #E27D60";
 
     gridSquare.addEventListener("mouseover", () => {
@@ -30,3 +34,12 @@ function makeGrid() {
 }
 
 makeGrid();
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    container.innerHTML = "";
+    makeGrid();
+  });
+});
